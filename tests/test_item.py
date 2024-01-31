a5aadf3b05faac4770ce0c3f6d7e02e373c446c4
Semyon Karaganov <zzzetmarley@gmail.com>
2024-01-31 12:05:16 +0700

    homework1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("apple", 10, 20)
+item2 = Item("potato", 5, 25)
+
+def test_calculate_total_price():
+    Item.pay_rate = 0.85
+
+    item1.apply_discount()
+    item2.apply_discount()
+    assert item1.calculate_total_price() == 170.0
+    assert item2.calculate_total_price() == 106.25
