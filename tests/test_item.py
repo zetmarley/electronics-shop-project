d53702a82e2b2f2d09417650c500881161496b8c
zetmarley <zzzetmarley@gmail.com>
2024-02-04 23:25:53 +0700

    homework3

--- tests/test_item.py
+++ tests/test_item.py
@@ -32,3 +32,8 @@ def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+def test_repr_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert str(item1) == 'Смартфон'
